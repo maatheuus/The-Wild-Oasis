f5ff1c75f3eac91ad78dd1c09cb0a2c5b29c759b
MATHEUS <matts14smkd@gmail.com>
2024-04-18 19:12:51 -0300

    Adicionando funcionalidades
    add múltiplas funcionalidades ao projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,64 @@
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
+
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <PageNotFound />,
+    children: [
+      { index: true, element: <Navigate replace to="dashboard" /> },
+      { path: "dashboard", element: <Dashboard /> },
+      { path: "bookings", element: <Bookings /> },
+      { path: "cabins", element: <Cabins /> },
+      { path: "users", element: <Users /> },
+      { path: "settings", element: <Settings /> },
+      { path: "account", element: <Account /> },
+    ],
+  },
+  { path: "login", element: <Login /> },
+]);
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   );
 }
 
 export default App;
+/**
+ *   defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000, // é quantidade de tempo que os dados ficaram armazenados até serem recarregados novamente
+    },
+  },
+ */
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -47,3 +47,21 @@ const variations = {
     }
   `,
 };
+
+const Button = styled.button`
+  margin: 1rem;
+
+  border: none;
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-sm);
+
+  ${(props) => variations[props.variation]}
+  ${(props) => sizes[props.size]}
+`;
+
+Button.defaultProps = {
+  variation: "primary",
+  size: "medium",
+};
+
+export default Button;
